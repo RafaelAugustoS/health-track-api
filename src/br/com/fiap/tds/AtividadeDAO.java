ad3a1df4f45465249d04e31fe7bbf86cb1afd925
Rafael Augusto <rafaelaugusto.developer@gmail.com>
2020-10-02 20:51:06 -0300

    change name

--- src/br/com/fiap/tds/AtividadeDAO.java
+++ src/br/com/fiap/tds/AtividadeDAO.java
@@ -2,7 +2,7 @@ package br.com.fiap.tds;
 
 public interface AtividadeDAO {
     public static String[] getAll(){
-        String[] cars = {"Bicicleta", "Correr", "Academia", "Aeróbica", "Natação", "Yoga", "Spinning", "Step", "Jump", "Crossfit", "Zumba"};
-        return cars;
+        String[] atividades = {"Bicicleta", "Correr", "Academia", "Aeróbica", "Natação", "Yoga", "Spinning", "Step", "Jump", "Crossfit", "Zumba"};
+        return atividades;
     }
 }
